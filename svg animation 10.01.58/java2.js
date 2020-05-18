dc57146c66e988af7ed702cf425dfb282bf04674
sebastian <seba6531@easv365.dk>
2020-05-18 10:03:37 +0200

    splash_screen
    stuff

---
+++ svg animation 10.01.58/java2.js
@@ -0,0 +1,9 @@
+var timeleft = 3;
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.querySelector('.animation').classList.add('coinFlip')
+    console.log('class has been added')
+  }
+  timeleft -= 1;
+}, 1000);
